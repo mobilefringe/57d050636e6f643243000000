6e057cb5d8de91826e48ee52500c0fc7af2a7641
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2016-09-08 15:53:09 -0400

    Rio Washingtonian 1.0  /site.js

---
+++ site.js
@@ -0,0 +1,7 @@
+var site_json = {
+                    "mall_name" : "RIO Washingtonian Center",
+                    "default_image" : "http://assets.codecloudapp.com/sites/57d050636e6f643243000000/image/png/1455899596000/main_logo.png",
+                    "time_zone" : "T05:00:00Z",
+                    "newsletter_form_url" : "http://petersoncompanies.createsend.com/t/d/s/amjk/?callback=?",
+                    "newsletter_email_field" : "cm-amjk-amjk"
+                }
